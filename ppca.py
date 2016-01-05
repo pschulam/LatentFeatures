df8a2a60a906410f6e0900ebe28fbe2679cd2fe9
Peter Schulam <pschulam@gmail.com>
2016-01-05 14:35:36 -0500

    Data structures for PPCA.

---
+++ ppca.py
@@ -0,0 +1,124 @@
+import numpy as np
+import scipy.linalg as la
+
+from scipy.stats import multivariate_normal as mvn
+
+
+_LOG_2PI = np.log(2 * np.pi)
+
+
+def learn_param(data, hid_dim):
+    '''Learn the parameters of probabilistic PCA.
+
+    returns : mean mu, loading matrix W, and variance sigsq.
+
+    '''
+    num_obs, obs_dim = data.shape
+    ppca = ProbabilisticPCA(obs_dim, hid_dim)
+
+
+def observed_loglik(dataset, ppca):
+    '''The observed-data log likelihood of the data.
+
+    '''
+    N = len(dataset)
+    D = ppca.obs_dim
+    C = ppca.marginal_cov()
+    P = ppca.marginal_prec()
+    S = dataset.scatter_matrix(center=ppca.marginal_mean())
+    return -N / 2 * (D * _LOG_2PI + np.log(la.det(C)) + trace(P * S))
+
+
+def simulate_data(num_obs, obs_dim, hid_dim, seed=0):
+    '''Generate a dataset to use as an example.
+
+    '''
+    rand = np.random.RandomState(seed)
+
+    subspace = rand.multivariate_normal(np.zeros(hid_dim), np.eye(hid_dim), size=obs_dim)
+    colvecs, singvals = la.svd(subspace, full_matrices=False)[:2]
+    loading = colvecs * singvals
+    
+    latent = rand.multivariate_normal(np.zeros(hid_dim), np.eye(hid_dim), size=num_obs)
+    observed = np.dot(latent, loading.T) + rand.normal(size=(num_obs, obs_dim))
+    
+    return loading, latent, observed
+
+
+class ProbabilisticPCA:
+    def __init__(self, obs_dim, hid_dim):
+        self.mean    = np.zeros(obs_dim)
+        self.loading = np.zeros((obs_dim, hid_dim))
+        self.var     = 1.0
+        self.obs_dim, self.hid_dim = self.loading.shape
+
+    def marginal_mean(self):
+        '''The mean of the marginal distribution.
+
+        '''
+        return self.mean.copy()
+
+    def marginal_cov(self):
+        '''The covariance matrix of the marginal distribution.
+
+        '''
+        WWt = np.dot(self.loading, self.loading.T)
+        return WWt + self.var * np.eye(len(WWt))
+
+    def marginal_prec(self):
+        '''The precision matrix of the marginal distribution.
+
+        '''
+        prec = np.eye(self.obs_dim) / np.sqrt(self.var)
+
+        W = self.loading
+        M = np.dot(W.T, W) + self.var * np.eye(self.hid_dim)
+        prec += np.dot(W, la.solve(M, W.T)) / self.var
+
+        return prec
+
+    def update_mean(self, empirical_mean):
+        self.mean[:] = empirical_mean
+
+
+class Dataset:
+    def __init__(self, data):
+        self.data = np.array(data)
+        self.mean, self.cov = Dataset._empirical_stats(self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def empirical_mean(self):
+        return self.mean
+
+    def empirical_cov(self):
+        return self.cov
+
+    def scatter_matrix(self, center):
+        centered = self.data - center
+        return np.dot(centered.T, centered) / len(self.data)
+
+    @staticmethod
+    def _empirical_stats(data):
+        mean = data.mean(axis=0)
+        centered_data = data - mean
+        cov = np.dot(centered_data.T, centered_data) / len(data)
+        return mean, cov
+
+
+def trace(A):
+    return np.diag(A).sum()
+
+
+if __name__ == '__main__':
+    num_obs = 1000
+    obs_dim = 6
+    hid_dim = 3
+    
+    W, Z, X = simulate_data(num_obs, obs_dim, hid_dim)
+    dataset = Dataset(X)
+    model = ProbabilisticPCA(obs_dim, hid_dim)
+    model.update_mean(dataset.empirical_mean())
+
+    print('LL={:.8f}'.format(observed_loglik(dataset, model)))
